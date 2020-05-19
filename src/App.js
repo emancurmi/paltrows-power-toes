054466f76dcb78aad5a7c193da7b0706645e3ffd
Eman Curmi <emancurmi@gmail.com>
2020-05-18 21:48:11 -0400

    conver menu to route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
 import Nav from './Nav'
 import Header from './Header'
 import Footer from './Footer'
 import HomePage from './HomePage'
+import AboutPage from './AboutPage'
+import WorkoutsPage from './WorkoutsPage'
+import NotFoundPage from './NotFoundPage'
 import './App.css'
 
 export default class App extends Component {
@@ -15,8 +19,14 @@ export default class App extends Component {
         <header>
           <Header />
         </header>
-        <main>
-          <HomePage />
+            <main>
+                <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/about' component={AboutPage} />
+                <Route path='/workouts' component={WorkoutsPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+
         </main>
         <footer>
           <Footer />
